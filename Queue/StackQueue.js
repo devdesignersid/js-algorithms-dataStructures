d00411dee02cefd045e223dcb666fe9dabe89b97
Siddharth <siddharth@Siddharths-MacBook-Pro.local>
2021-10-19 15:52:49 +0530

    queue implemented

--- Queue/StackQueue.js
+++ Queue/StackQueue.js
@@ -5,6 +5,20 @@ module.exports = class Queue {
     this.stackOne = [];
     this.stackTwo = [];
   }
-  enqueue = (value) => {};
-  dequeue = () => {};
+  enqueue = (value) => {
+    for (let i = 0; i < this.stackTwo.length; i++) {
+      this.stackOne.push(this.stackTwo.pop());
+    }
+    this.stackOne.push(value);
+    return this.stackOne;
+  };
+  dequeue = () => {
+    while (this.stackOne.length > 0) {
+      this.stackTwo.push(this.stackOne.pop());
+    }
+    return this.stackTwo.pop();
+  };
+  peek = () => (this.stackOne.length > 0 ? this.stackOne[0] : this.stackTwo[0]);
+
+  isEmpty = () => (this.stackOne.length ? false : true);
 };
--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const Queue = require("./Queue/Queue");
+const Queue = require("./Queue/StackQueue");
 
 const queue = new Queue();
 console.log("is queue empty: ", queue.isEmpty());
 queue.enqueue(2);
 queue.enqueue(3);
 queue.enqueue(4);
-queue.linkedList.prettyPrint();
 console.log("first element: ", queue.peek());
 queue.dequeue();
 queue.dequeue();
 queue.dequeue();
 queue.dequeue();
-queue.linkedList.prettyPrint();
 console.log("first element: ", queue.peek());
 console.log("is queue empty: ", queue.isEmpty());
